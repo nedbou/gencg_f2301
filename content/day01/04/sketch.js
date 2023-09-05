f4edd580aeaba29d5d9025b095a5dca5ef038934
nedia <nadia.boutouchent@stud.hslu.ch>
2023-09-05 12:07:54 +0200

    new folder04

---
+++ content/day01/04/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+rect(30, 50, 55, 55);
+  triangle(30, 50, 58, 20, 86, 50)
+  line(30, 50, 85, 105)
+  line(30, 105, 85, 50)
+  
+}
+function setup() {
+  createCanvas(400, 400);
+  ellipse(70, 100, 120, 30);
+      beginShape();
+      vertex(26, 50);
+      vertex(115, 50);
+      vertex(100, 100);
+      vertex(40,100 );
+      endShape(CLOSE);  
+  
+  ellipse(70, 46, 90, 30);
+  
+  function setup() {
+  createCanvas(400, 400);
+  rect(100,100,10,150)
+    rect(35,100,150,10)
+  quad(quad(35, 100, 184, 100, 150, 80, 70, 80))
+  
+  
+  
+  
+}
