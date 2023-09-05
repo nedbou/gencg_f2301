8af70f2e106eb34f44020d650a2406ad8da740a8
nedia <nadia.boutouchent@stud.hslu.ch>
2023-09-05 15:35:33 +0200

    changed setup canvas

--- content/day01/04/sketch.js
+++ content/day01/04/sketch.js
@@ -1,13 +1,12 @@
-let canvas video
 
-function setup() {
+  function setup() {
   createCanvas(500, 500);
+  
   rect(30, 50, 55, 55);
   triangle(30, 50, 58, 20, 86, 50);
   line(30, 50, 85, 105);
   line(30, 105, 85, 50);
-}
-  function draw() {
+
   ellipse(70, 200, 120, 30);
   beginShape();
   vertex(26, 150);
@@ -15,13 +14,10 @@ function setup() {
   vertex(100, 200);
   vertex(40, 200);
   endShape(CLOSE);
-
   ellipse(70, 146, 90, 30);
 
-}
-  function draw() {
-    rect(100, 350, 10, 150);
-    rect(35, 350, 150, 10);
-    quad(35, 350, 184, 350, 150, 320, 70, 320)
+  rect(250, 150, 10, 150);
+  rect(185, 150, 150, 10);
+  quad(185, 150, 335, 150, 300, 110, 220, 110)
 
 }
