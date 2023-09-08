31b72b1e6c978488a8772431dea206a6cc45e2f2
nedia <nadia.boutouchent@stud.hslu.ch>
2023-09-08 17:26:29 +0200

    day04

---
+++ content/day04/02/sketch.js
@@ -0,0 +1,40 @@
+let mic;
+let fft;
+let isDrawing = false;
+let prevX, prevY;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  mic = new p5.AudioIn();
+  mic.start();
+  mic.amp(1); // Adjust the microphone input volume as needed
+  fft = new p5.FFT();
+  fft.setInput(mic); // Set the microphone as input for FFT
+  background(220);
+}
+
+function draw() {
+  let vol = mic.getLevel();
+  let spectrum = fft.analyze();
+  let mappedStrokeWeight = map(vol, 0, 0.1, 1, 10); // Adjust the stroke weight based on microphone input
+  strokeWeight(mappedStrokeWeight);
+  stroke(0); // Black color
+
+  if (isDrawing) {
+    line(prevX, prevY, mouseX, mouseY);
+  }
+
+  prevX = mouseX;
+  prevY = mouseY;
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    isDrawing = !isDrawing; // Toggle drawing on Enter key press
+    if (!isDrawing) {
+      // Clear the canvas when you stop drawing
+      background(220);
+    }
+  }
+}
+
